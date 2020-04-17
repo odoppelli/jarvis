743468d3d5fe83b32bce95777f63a4de31fa9426
Raspi <oliver.kuipers@alumni.fh-aachen.de>
2020-04-17 18:59:12 +0200

    radio for raspberry pi

--- radio.py
+++ radio.py
@@ -1,9 +1,29 @@
 import vlc
 import time
-url = 'http://streams.radiobob.de/bob-live/mp3-192/mediaplayer'
+#url = 'http://streams.radiobob.de/bob-live/mp3-192/mediaplayer'
+url = 'http://evans.hochschulradio.rwth-aachen.de:8000/radio_low.mp3'
 instance = vlc.Instance('--input-repeat=-1', '--fullscreen')
 player=instance.media_player_new()
 media=instance.media_new(url)
 media.get_mrl()
 player.set_media(media)
-player.play()
+
+def play():
+    player.play()
+
+def pause():
+    player.pause()
+
+def stop():
+    player.pause()
+    player.stop()
+
+# EXAMPLE
+play()
+time.sleep(10)
+pause()
+time.sleep(5)
+play()
+time.sleep(5)
+stop()
+print('beendet')

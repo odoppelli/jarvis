b1c72dd07fd7f381d36c347536c16d86839b1ea8
Oliver Kuipers <oliver.kuipers@alumni.fh-aachen.de>
2020-04-18 12:15:24 +0200

    update radio.py
    added url list

--- radio.py
+++ radio.py
@@ -1,29 +1,63 @@
 import vlc
 import time
-#url = 'http://streams.radiobob.de/bob-live/mp3-192/mediaplayer'
-url = 'http://evans.hochschulradio.rwth-aachen.de:8000/radio_low.mp3'
-instance = vlc.Instance('--input-repeat=-1', '--fullscreen')
-player=instance.media_player_new()
-media=instance.media_new(url)
-media.get_mrl()
-player.set_media(media)
 
-def play():
+action_play = False
+action_pause = False
+action_stop = False
+action_next = False
+action_previous = False
+
+def play_radio():
     player.play()
 
-def pause():
+def pause_radio():
     player.pause()
 
-def stop():
+def stop_radio():
     player.pause()
     player.stop()
 
+def next_radio():
+    player.next()
+
+hochschulradio = 'http://evans.hochschulradio.rwth-aachen.de:8000/radio_low.mp3'
+club_musik = 'http://club-high.rautemusik.fm/'
+url_list = [hochschulradio, club_musik]
+
+
+instance = vlc.Instance('--input-repeat=-1', '--fullscreen')
+
+MediaList = instance.media_list_new()
+for url in url_list:
+    MediaList.add_media(instance.media_new(url))
+
+list_player = instance.media_list_player_new()
+list_player.set_media_list(MediaList)
+
+
+'''
+player = instance.media_player_new()
+media = instance.media_new(url)
+player.set_media_list(url_list)
+media.get_mrl()
+player.set_media(media)
+player.play()
+'''
+
+
+
 # EXAMPLE
-play()
+play_radio()
 time.sleep(10)
-pause()
+pause_radio()
 time.sleep(5)
-play()
-time.sleep(5)
-stop()
+play_radio()
+time.sleep(10)
+next_radio()
+time.sleep(10)
+play_radio(10)
+time.sleep(10)
+stop_radio()
+
+
 print('beendet')

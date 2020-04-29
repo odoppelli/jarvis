f5366096a8ee73d8a4d68cd9e88636332b71c0f2
Oliver Kuipers <oliver.kuipers@alumni.fh-aachen.de>
2020-04-29 19:07:29 +0200

    added other audiobooks
    - Känguru Manifest|Offenbarung|Apokryphen
    - The Hitchhikers Guide to the Galaxy

--- player.py
+++ player.py
@@ -57,27 +57,123 @@ class KanguruChroniken(WeckerPlayer):
             '1': "/home/pi/Audiobooks/Die Känguru Chroniken/Marc-Uwe Kling- Die Känguru-Chroniken. CD 1",
             '2': "/home/pi/Audiobooks/Die Känguru Chroniken/Marc-Uwe Kling- Die Känguru-Chroniken. CD 2",
             '3': "/home/pi/Audiobooks/Die Känguru Chroniken/Marc-Uwe Kling- Die Känguru-Chroniken. CD 3",
-            '4': "/home/pi/Audiobooks/Die Känguru Chroniken/Marc-Uwe Kling- Die Känguru-Chroniken. CD 4",
-
+            '4': "/home/pi/Audiobooks/Die Känguru Chroniken/Marc-Uwe Kling- Die Känguru-Chroniken. CD 4"
             }
         d = cds.get(str(cd_number))
         kanguru_paths = []
         for path in os.listdir(d):
-            full_path = os.path.join(d,path)
+            full_path = os.path.join(d, path)
             if os.path.isfile(full_path):
                 kanguru_paths.append(full_path)
-                
         super().__init__(kanguru_paths)
 
-# BEISPIEL
-#wecker = WeckerRadio()
-#wecker.play_media()
-#time.sleep(20)
-#wecker.stop_media()
 
-kanguru = KanguruChroniken(1)
-kanguru.play_media()
+class KanguruManifest(WeckerPlayer):
+    def __init__(self, cd_number):
+        cds = {
+            '1': "/home/pi/Audiobooks/Das Känguru Manifest/Marc-Uwe Kling- Das Känguru-Manifest. CD 1",
+            '2': "/home/pi/Audiobooks/Das Känguru Manifest/Marc-Uwe Kling- Das Känguru-Manifest. CD 2",
+            '3': "/home/pi/Audiobooks/Das Känguru Manifest/Marc-Uwe Kling- Das Känguru-Manifest. CD 3",
+            '4': "/home/pi/Audiobooks/Das Känguru Manifest/Marc-Uwe Kling- Das Känguru-Manifest. CD 4"
+            }
+        d = cds.get(str(cd_number))
+        kanguru_paths = []
+        for path in os.listdir(d):
+            full_path = os.path.join(d, path)
+            if os.path.isfile(full_path):
+                kanguru_paths.append(full_path)
+        super().__init__(kanguru_paths)
+
+
+class KanguruOffenbarung(WeckerPlayer):
+    def __init__(self, cd_number):
+        cds = {
+            '1': "/home/pi/Audiobooks/Die Känguru Offenbarung/Die Känguru-Offenbarung- Live Und Ungekürzt [Disc 1]",
+            '2': "/home/pi/Audiobooks/Die Känguru Offenbarung/Die Känguru-Offenbarung- Live Und Ungekürzt [Disc 2]",
+            '3': "/home/pi/Audiobooks/Die Känguru Offenbarung/Die Känguru-Offenbarung- Live Und Ungekürzt [Disc 3]",
+            '4': "/home/pi/Audiobooks/Die Känguru Offenbarung/Die Känguru-Offenbarung- Live Und Ungekürzt [Disc 4]",
+            '5': "/home/pi/Audiobooks/Die Känguru Offenbarung/Die Känguru-Offenbarung- Live Und Ungekürzt [Disc 5]",
+            '6': "/home/pi/Audiobooks/Die Känguru Offenbarung/Die Känguru-Offenbarung- Live Und Ungekürzt [Disc 6]"
+        }
+        d = cds.get(str(cd_number))
+        kanguru_paths = []
+        for path in os.listdir(d):
+            full_path = os.path.join(d, path)
+            if os.path.isfile(full_path):
+                kanguru_paths.append(full_path)
+        super().__init__(kanguru_paths)
+
+
+class KanguruApokryphen(WeckerPlayer):
+    def __init__(self, cd_number):
+        cds = {
+            '1': "/home/pi/Audiobooks/Die Känguru Apokryphen/CD 1",
+            '2': "/home/pi/Audiobooks/Die Känguru Apokryphen/CD 2",
+            '3': "/home/pi/Audiobooks/Die Känguru Apokryphen/CD 3",
+            '4': "/home/pi/Audiobooks/Die Känguru Apokryphen/CD 4"
+        }
+        d = cds.get(str(cd_number))
+        kanguru_paths = []
+        for path in os.listdir(d):
+            full_path = os.path.join(d, path)
+            if os.path.isfile(full_path):
+                kanguru_paths.append(full_path)
+        super().__init__(kanguru_paths)
+
+class HitchhikersGuide(WeckerPlayer):
+    def __init__(self, cd_number):
+        cds = {
+            '1': "/home/pi/Audiobooks/The Hitchhikers Guide to the Galaxy/The Hitchhiker's Guide To The Galaxy [Disc 1]",
+            '2': "/home/pi/Audiobooks/The Hitchhikers Guide to the Galaxy/The Hitchhiker's Guide To The Galaxy [Disc 2]",
+            '3': "/home/pi/Audiobooks/The Hitchhikers Guide to the Galaxy/The Hitchhiker's Guide To The Galaxy [Disc 3]",
+            '4': "/home/pi/Audiobooks/The Hitchhikers Guide to the Galaxy/The Hitchhiker's Guide To The Galaxy [Disc 4]"
+        }
+        d = cds.get(str(cd_number))
+        kanguru_paths = []
+        for path in os.listdir(d):
+            full_path = os.path.join(d, path)
+            if os.path.isfile(full_path):
+                kanguru_paths.append(full_path)
+        super().__init__(kanguru_paths)
+
+
+# BEISPIEL | TEST
+wecker = WeckerRadio()
+wecker.play_media()
 time.sleep(20)
-kanguru.next_media()
+wecker.stop_media()
+
+kanguru1 = KanguruChroniken(1)
+kanguru1.play_media()
 time.sleep(20)
-kanguru.stop_media()
+kanguru1.next_media()
+time.sleep(20)
+kanguru1.stop_media()
+
+kanguru2 = KanguruManifest(1)
+kanguru2.play_media()
+time.sleep(10)
+kanguru2.next_media()
+time.sleep(10)
+kanguru2.stop_media()
+
+kanguru3 = KanguruOffenbarung(1)
+kanguru3.play_media()
+time.sleep(10)
+kanguru3.next_media()
+time.sleep(10)
+kanguru3.stop_media()
+
+kanguru4 = KanguruApokryphen(1)
+kanguru4.play_media()
+time.sleep(10)
+kanguru4.next_media()
+time.sleep(10)
+kanguru4.stop_media()
+
+hitchhiker = HitchhikersGuide(1)
+hitchhiker.play_media()
+time.sleep(10)
+hitchhiker.next_media()
+time.sleep(10)
+hitchhiker.stop_media()
